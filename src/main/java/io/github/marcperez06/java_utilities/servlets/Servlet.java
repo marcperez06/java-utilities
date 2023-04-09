e18d08ca6620ff50c3c6f3f7a1ef99b6e59c56a8
marcperez06 <marc.perez.rodriguez0@gmail.com>
2023-04-09 21:55:36 +0200

    Fix import servlet issues

--- src/main/java/io/github/marcperez06/java_utilities/servlets/Servlet.java
+++ src/main/java/io/github/marcperez06/java_utilities/servlets/Servlet.java
@@ -6,11 +6,11 @@ package io.github.marcperez06.java_utilities.servlets;
 import java.io.File;
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class Servlet extends HttpServlet {
 
